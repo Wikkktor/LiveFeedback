ddd8f4df6e531036dec7b85f02a8d4a7270020de
Wikkktor <wkaraszewicz@gmail.com>
2025-05-13 20:41:34 +0200

    Remove topic - it is handled dynamicaly based on the model name

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 KAFKA_BOOTSTRAP_SERVERS: str = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
-KAFKA_TOPIC: str = os.getenv("KAFKA_TOPIC", "feedback-topic")
 KAFKA_GROUP_ID: str = os.getenv("KAFKA_GROUP_ID", "feedback-group")
 
 ELASTIC_HOST: str = os.getenv("ELASTIC_HOST", "http://localhost:9200")
